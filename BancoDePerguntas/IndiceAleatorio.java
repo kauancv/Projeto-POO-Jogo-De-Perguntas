db4594969278361f43484a92964ca13e98a21631
KauanPc <kauancv239@gmail.com>
2023-11-02 11:18:24 -0300

    Teste com classe Banco de Perguntas e Criacao da Classe Indice

---
+++ BancoDePerguntas/IndiceAleatorio.java
@@ -0,0 +1,13 @@
+package BancoDePerguntas;
+
+import java.util.ArrayList;
+
+public class IndiceAleatorio {
+    ArrayList <Integer> indice = new ArrayList<>();
+
+     public void preencherArrayList(){
+        for (int i = 0; i < Perguntas.tamanhoArrayList();i++){
+            
+        }
+     }
+}
